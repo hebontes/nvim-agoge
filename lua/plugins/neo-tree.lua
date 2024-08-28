4a4a4f90165988e6119e8c829461c57cf35b3c16
Your Name <you@example.com>
2024-08-28 11:10:15 +0400

    feat: added variable names reference text

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,7 @@ return {
   },
   config = function()
 
-    vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>')
+    --vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>')
+    vim.keymap.set('n', '<leader>n', ':Neotree filesystem toggle<CR>')
   end
 }
