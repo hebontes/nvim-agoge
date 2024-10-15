8367053061f505136e1a6a50f2c43f699a7e4eee
Your Name <you@example.com>
2024-10-15 09:47:20 +0400

    feat: clangd for cpp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,7 +19,7 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
-          "ast_grep",
+          "clangd"
         }
       })
     end
@@ -29,7 +29,8 @@ return {
     config = function()
       local lspconfig = require('lspconfig')
       lspconfig.lua_ls.setup({})
-      lspconfig.ast_grep.setup({})
+      lspconfig.clangd.setup({})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
     end
   }
 }
