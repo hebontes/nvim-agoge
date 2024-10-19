929a947240fee452725d04ffd6e2a9fdfd13d88d
Your Name <you@example.com>
2024-10-19 23:02:54 +0400

    added hardtime lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,54 +1,28 @@
---[[return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope-frecency.nvim',
-    'kkharji/sqlite.lua',
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',  
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    end
   },
-  config = function()
-    local telescope = require("telescope")
-    local builtin = require("telescope.builtin")
-
-    -- Configure Telescope
-    telescope.setup({
-      extensions = {
-        frecency = {
-          show_scores = true,
-          show_unindexed = true,
-          ignore_patterns = {"*.git/*", "*/tmp/*"},
-          disable_devicons = false,
-          workspaces = {
-            ["conf"]    = "/home/user/.config",
-            ["data"]    = "/home/user/.local/share",
-            ["project"] = "/home/user/projects",
-            ["wiki"]    = "/home/user/wiki"
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config  = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
           }
         }
       }
-    })
-
-    -- Load the frecency extension
-    telescope.load_extension("frecency")
-
-    -- Keymaps
-    vim.keymap.set('n', '<leader>ff', function()
-      telescope.extensions.frecency.frecency()
-    end, { noremap = true, silent = true })
-    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-  end
+      require("telescope").load_extension("ui-select")
+    end
+  }
 }
---]]
 
-return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',  
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-  end
-}
