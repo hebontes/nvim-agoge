77bbb13f5b7af2a6eeca13ffbabb19229b7e7a3c
Your Name <you@example.com>
2024-10-19 23:12:59 +0400

    added C++/C server to LSP

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -4,5 +4,6 @@ return {
   opts = {},
   config = function()
     require("hardtime").setup()
+    vim.keymap.set('n', '<leader>ht', ':Hardtime toggle<CR>', {})
   end
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,6 +19,7 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
+          "clangd",
         }
       })
     end
@@ -28,6 +29,7 @@ return {
     config = function()
       local lspconfig = require('lspconfig')
       lspconfig.lua_ls.setup({})
+      lspconfig.clangd.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
